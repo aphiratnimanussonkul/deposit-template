04d4d2bf7bfe152aef34c4201cee7b0a01d99194
Aphirat Nimanussonkul <b5923151@gmail.com>
2023-08-11 16:57:53 +0700

    [Init Project]

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <a
+          href="mailto:lessex@odds.team.com?subject=test send mail from electron&body=TThe message"
+          target="_blank"
+        >
+          <button id="btnOutlook">Go to Outlook</button>
+        </a>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
